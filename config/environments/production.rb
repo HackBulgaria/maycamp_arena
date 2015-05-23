6c738e2c4dfed9dc34153276badd3b94db1360ea
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-05-23 20:05:40 +0300

    Send mails through arena.hackbulgaria.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,5 +57,5 @@ MaycampArena::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => "arena.maycamp.com" }
+  config.action_mailer.default_url_options = { :host => "arena.hackbulgaria.com" }
 end
